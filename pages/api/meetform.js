244ad4a741a1ffb05e61158505e05252473218d9
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-07-22 13:52:34 +0530

    modification

--- pages/api/meetform.js
+++ pages/api/meetform.js
@@ -1,19 +1,38 @@
 import { MongoClient } from "mongodb";
+
 async function handler(req, res) {
   const username = "ashishchaudhari1857";
-  const password = "pass@123"; 
+  const password = "pass@123";
 
   if (req.method === "POST") {
     const data = req.body;
-    const client = await MongoClient.connect(
-      `mongodb+srv://${username}:${encodeURIComponent(password)}@cluster0.nuhh21u.mongodb.net/meetups?retryWrites=true&w=majority`
-    );
-    const db = client.db();
-    const meetupscollections = db.collection("meetups");
-    const result = await meetupscollections.insertOne(data);
-    console.log(result);
-    client.close();
-    res.status(201).json({ message: "meetup inserted" });
+
+    try {
+      const client = await MongoClient.connect(
+        `mongodb+srv://${username}:${encodeURIComponent(password)}@cluster0.nuhh21u.mongodb.net/meetups?retryWrites=true&w=majority`
+      );
+      const db = client.db();
+      const meetupscollections = db.collection("meetups");
+
+      // Insert the new meetup data into the database
+      const result = await meetupscollections.insertOne(data);
+
+      // Check if the insertion was successful  this is  the inser massage  don go before insrting
+      
+      if (result.insertedCount === 1) {
+        console.log("Meetup inserted:", result.insertedId);
+        client.close();
+        res.status(201).json({ message: "Meetup inserted successfully" });
+      } else {
+        // In case of unsuccessful insertion
+        console.log("Meetup insertion failed");
+        client.close();
+        res.status(500).json({ message: "Failed to insert meetup" });
+      }
+    } catch (err) {
+      console.log("Error:", err);
+      res.status(500).json({ message: "Server error" });
+    }
   }
 }
 
